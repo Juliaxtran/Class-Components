542f097f81cc69601110767a058c1e0c71cf34f0
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-15 10:51:04 -0500

    Setting state in class components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-
-import './App.css';
-import React from 'react';
-
-// function App(props) {
-//   return (
-//     <div className="App">
-//      <h1>{props.type}</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
+import React from "react"
+import './App.css'
 
 export default class App extends React.Component {
-    render () {
+    // const [goOut, setGoOut] = React.useState("Yes")
+
+    state = {
+        goOut: "Yes"
+    }
+
+    toggleGoOut = () => {
+        this.setState(prevState => {
+            return {
+              goOut: prevState.goOut === "Yes" ? "No" : "Yes"
+            }
+        })
+    }
+
+    render() {
         return (
-            <div className="App">
-                <h1>{this.props.type}components</h1>
+            <div className="state">
+                <h1 className="state--title">Should I go out tonight?</h1>
+                <div className="state--value" onClick={this.toggleGoOut}>
+                    <h1>{this.state.goOut}</h1>
+                </div>
             </div>
-        );
+        )
     }
 }

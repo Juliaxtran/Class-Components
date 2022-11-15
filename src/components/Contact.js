0ba27ed4089f7105bf2775a3a7225a03619bb0cd
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-15 11:39:42 -0500

    Updated complex state with this.setSate

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,48 @@
 import React from "react"
 import './Contact.css'
 import user from "../images/user.png"
+import starFilled from "../images/star-filled.png"
+import starEmpty from "../images/star-empty.png"
 
 
-export default function Contact() {
-    const [contact, setContact] = React.useState({
+export default class Contact extends React.Component {
+    state = {
         firstName: "John",
         lastName: "Doe",
         phone: "+1 (719) 555-1212",
         email: "itsmyrealname@example.com",
         isFavorite: false
-    })
+    }
 
-    let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
 
-    function toggleFavorite() {
-        setContact(prevContact => ({
-            ...prevContact,
+ toggleFavorite = () =>  {
+        this.setState(prevContact => ({
             isFavorite: !prevContact.isFavorite
         }))
     }
 
-    return (
+    render () {
+      let starIcon = this.state.isFavorite ? {starFilled} : {starEmpty}
+      return (
         <main>
             <article className="card">
                 <img src={user} className="card--image" />
                 <div className="card--info">
                     <img
-                        src={`../images/${starIcon}`}
+                        src={starIcon}
                         className="card--favorite"
-                        onClick={toggleFavorite}
+                        onClick={this.toggleFavorite}
                     />
                     <h2 className="card--name">
-                        {contact.firstName} {contact.lastName}
+                        {this.state.firstName} {this.state.lastName}
                     </h2>
-                    <p className="card--contact">{contact.phone}</p>
-                    <p className="card--contact">{contact.email}</p>
+                    <p className="card--contact">{this.state.phone}</p>
+                    <p className="card--contact">{this.stateemail}</p>
                 </div>
 
             </article>
         </main>
     )
+    }
+
 }

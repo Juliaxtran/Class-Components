d0ca60f93de3b916d3df623100ace83272470c92
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-15 10:27:25 -0500

    Intro to class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+// function App(props) {
+//   return (
+//     <div className="App">
+//      <h1>{props.type}</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default class App extends React.Component {
+    render () {
+        return (
+            <div className="App">
+                <h1>{this.props.type}components</h1>
+            </div>
+        );
+    }
 }
-
-export default App;
